e294724a05c030734180a8f0c5c922d56acb7f5d
caleb <andriambolaradoniainamichael@gmail.com>
2024-07-04 15:23:23 +0300

    fix validation contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,7 +1,27 @@
+'use client';
 import contact1 from '@/images/contact2.png';
 import decoration from '@/images/decoration-star.svg';
+import { contactSchema } from '@/utils/yup/Schema';
+import { useFormik } from 'formik';
 import Image from 'next/image';
+const initialValues = {
+  name: '',
+  email: '',
+  message: '',
+  phone: '',
+};
 function Contact(props) {
+  function onSubmit() {
+    console.log(values);
+    resetForm();
+  }
+  const { handleSubmit, handleChange, touched, errors, values, resetForm } =
+    useFormik({
+      initialValues,
+      validationSchema: contactSchema,
+      onSubmit,
+    });
+
   return (
     <section id='contact' className='contact my-2 position-relative'>
       <Image
@@ -19,34 +39,84 @@ function Contact(props) {
           <div className='col-lg-6'>
             <div className='text-container'>
               <h2>Laissez-nous un message</h2>
-              <form>
+              <form onSubmit={handleSubmit} autoComplete='off'>
                 <div className='mb-3'>
                   <input
                     type='text'
-                    className='form-control'
-                    placeholder='Enter name'
+                    className={`form-control ${
+                      touched.name && errors.name ? 'border border-danger' : ''
+                    }`}
+                    placeholder='nom'
+                    name='name'
+                    id='name'
+                    value={values.name}
+                    onChange={handleChange}
                   />
+                  {touched.name && errors.name ? (
+                    <div className='text-danger ps-2'>{errors.name}</div>
+                  ) : (
+                    ''
+                  )}
                 </div>
                 <div className='mb-3'>
                   <input
                     type='email'
-                    className='form-control'
-                    placeholder='Enter email'
+                    className={`form-control ${
+                      touched.email && errors.email
+                        ? 'border border-danger'
+                        : ''
+                    }`}
+                    placeholder='email'
+                    name='email'
+                    id='email'
+                    value={values.email}
+                    onChange={handleChange}
                   />
+                  {touched.email && errors.email ? (
+                    <div className='text-danger ps-2'>{errors.email}</div>
+                  ) : (
+                    ''
+                  )}
                 </div>
                 <div className='mb-3'>
                   <input
                     type='text'
-                    className='form-control'
+                    className={`form-control ${
+                      touched.phone && errors.phone
+                        ? 'border border-danger'
+                        : ''
+                    }`}
                     placeholder='Enter phone'
+                    name='phone'
+                    id='phone'
+                    value={values.phone}
+                    onChange={handleChange}
                   />
+                  {touched.phone && errors.phone ? (
+                    <div className='text-danger ps-2'>{errors.phone}</div>
+                  ) : (
+                    ''
+                  )}
                 </div>
                 <div className='mb-3'>
                   <textarea
-                    className='form-control'
+                    className={`form-control ${
+                      touched.message && errors.message
+                        ? 'border border-danger'
+                        : ''
+                    }`}
                     placeholder='Enter message'
                     rows='8'
+                    name='message'
+                    id='message'
+                    value={values.message}
+                    onChange={handleChange}
                   ></textarea>
+                  {touched.message && errors.message ? (
+                    <div className='text-danger ps-2'>{errors.message}</div>
+                  ) : (
+                    ''
+                  )}
                 </div>
                 <div className='d-grid gap-2'>
                   <input
--- src/utils/yup/Schema.js
+++ src/utils/yup/Schema.js
@@ -1,3 +1,4 @@
+import * as yup from 'yup';
 import { string } from 'yup';
 const emailRegex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 export const contactSchema = yup.object().shape({
@@ -5,19 +6,17 @@ export const contactSchema = yup.object().shape({
     .min(4, "Il est nécessaire d'avoir au moins 4 caractères.")
     .required('Veuillez entrer votre nom'),
   email: string()
-    .matches(
-      /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
-      'Veuillez entrer une adresse e-mail valide.'
-    )
+    .matches(emailRegex, 'Veuillez entrer une adresse e-mail valide.')
     .required('Veuillez entrer votre adresse e-mail.'),
   phone: string()
+    .min(10, "Il est nécessaire d'avoir 10 caractères")
+    .max(10, "Il est nécessaire d'avoir 10 caractères")
     .matches(
       /^[+]*[(]{0,1}[0-9]{1,4}[)]{0,1}[-\s\./0-9]*$/g,
       'Veuillez entrer un numéro de téléphone valide'
     )
     .required('Veuillez entrer votre numéro de  téléphone.'),
-  message: string().min(
-    20,
-    "Il est nécessaire d'avoir au moins 20 caractères."
-  ),
+  message: string()
+    .min(20, "Il est nécessaire d'avoir au moins 20 caractères.")
+    .required('Veuillez entrer votre message'),
 });
