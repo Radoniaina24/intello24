36d900d853d4fa1240ffc5d18cff8b1fac472df3
caleb <andriambolaradoniainamichael@gmail.com>
2024-07-09 10:12:28 +0300

    add animation slogan

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,27 @@
+'use client';
 import Link from 'next/link';
-
+import { TypeAnimation } from 'react-type-animation';
 export default function Hero() {
   return (
     <>
       <header class='header position-relative'>
-        {/* <Image
-        src='images/vertical-decoration-left.svg'
-        alt=''
-        className='vertical-decoration position-absolute d-none d-md-block'
-      /> */}
         <div class='container'>
           <div class='row text-center'>
             <div class='col-12'>
               <h1 class='xl-text'>
-                La <span className='text-primary fw-bold'>réussite</span>{' '}
-                n&apos;a rien de magique. Elle se prépare.
+                La <span className='text-primary fw-bold'>réussite </span>
+                <TypeAnimation
+                  sequence={[
+                    // Same substring at the start will only be typed out once, initially
+                    "n'a rien de magique. Elle se prépare.",
+                    1000,
+                  ]}
+                  wrapper='span'
+                  speed={50}
+                  style={{}}
+                  repeat={Infinity}
+                  cursor={false}
+                />
               </h1>
               <Link href='/about' class='btn btn-primary text-white mt-4'>
                 Découvrir le collège
