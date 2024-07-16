f98741f1e7d676bc660f2923476bc001a2132f28
Your Name <you@example.com>
2024-07-16 13:57:26 +0300

    fix hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,15 @@
 'use client';
 import Image from 'next/image';
-import Link from 'next/link';
 import { TypeAnimation } from 'react-type-animation';
-import hero from '@/images/hero.png'
 import Svg from './Svg';
+import focused from '@/images/focused.png'
 export default function Hero() {
   return (
     <>
       <header className='header position-relative'>
         <div className='container py-8 '>
-          <div className='row text-center align-items-center'>
-            <div className="col-md-4">
+          <div className='row text-center align-items-center justify-content-center'>
+            <div className="col-md-6">
               <h1 className='text-white'>
                 La <span className='text-primary fw-bold'>réussite </span>
                 <TypeAnimation
@@ -21,15 +20,14 @@ export default function Hero() {
                   ]}
                   wrapper='span'
                   speed={50}
-                  style={{}}
                   repeat={false}
                   cursor={false}
                 />
               </h1>
             </div>
-            <div className="col-md-8">
-              <Image src={hero} alt="hero image" className='img-fluide  w-75 h-75'/>
-            </div>
+            {/* <div className="col-md-8">
+              <Image src={focused} alt='study' className='img-fluid w-75 h-75'/>
+            </div> */}
           </div>
         </div>
         <Svg/>
@@ -37,23 +35,3 @@ export default function Hero() {
     </>
   );
 }
-/**<div className='col-12'>
-              <h1 className='xl-text text-white'>
-                La <span className='text-primary fw-bold'>réussite </span>
-                <TypeAnimation
-                  sequence={[
-                    // Same substring at the start will only be typed out once, initially
-                    "n'a rien de magique. Elle se prépare.",
-                    1000,
-                  ]}
-                  wrapper='span'
-                  speed={50}
-                  style={{}}
-                  repeat={false}
-                  cursor={false}
-                />
-              </h1>
-              <Link href='/about' className='btn btn-primary text-white mt-4'>
-                En savoir plus
-              </Link>
-            </div> */
