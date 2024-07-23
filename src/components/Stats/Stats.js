41a4286d18e5d32a0b7cc8cff8b66cca7635de18
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2024-07-23 10:06:11 +0300

    Add statt

--- src/components/Stats/Stats.js
+++ src/components/Stats/Stats.js
@@ -2,8 +2,8 @@
 import CountUp from 'react-countup';
 function Stats(props) {
   return (
-    <section id='stats' className='stats'>
-      <div className='row my-6 ' data-aos="zoom-in-down" data-aos-duration="4000">
+    <section id='stats' className='stats d-flex justify-content-center'>
+      <div className='row my-6 container' data-aos="zoom-in-down" data-aos-duration="4000">
         <div className='col-md-4 col-sm-6 text-center  border-end border-4 border-light my-5' data-aos="zoom-in-down" data-aos-duration="4000">
           <h2 className='counter xl-text'>
             +<CountUp start={0} end={328} duration={30} />
@@ -18,11 +18,11 @@ function Stats(props) {
           <h6>Abonnés Facebook</h6>
         </div>
 
-        <div className='col-md-4 col-sm-6 text-center my-5' data-aos="zoom-in-down" data-aos-duration="4000">
+        <div className='col-md-4 col-sm-6 text-center my-5 ' data-aos="zoom-in-down" data-aos-duration="4000">
           <h2 className='counter xl-text'>
             +<CountUp start={0} end={159} duration={30} />
           </h2>
-          <h6>Bonnes critiques</h6>
+          <h6>Etudiants Diplomés</h6>
         </div>
       </div>
     </section>
