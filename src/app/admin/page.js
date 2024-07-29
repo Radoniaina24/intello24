55ae5e2911aa5ebab18080c8de3ae14bd9725147
Radoniaina24 <andriambolaradoniainamichael@gmail.com>
2024-07-29 10:14:53 +0300

    add page admin

---
+++ src/app/admin/page.js
@@ -0,0 +1,3 @@
+export default function Admin() {
+  return <div className="admin my-6">Admin</div>;
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,29 @@
-import Contact from '@/components/Contact/Contact';
-import Details from '@/components/Details/Details';
+import Contact from "@/components/Contact/Contact";
+import Details from "@/components/Details/Details";
 // import Details from '@/components/Details/Details';
-import Details2 from '@/components/Details/Details2';
-import Hero from '@/components/Hero/Hero';
-import Slogan from '@/components/Slogan/Slogan';
-import Stats from '@/components/Stats/Stats';
-import Systeme from '@/components/Systeme/Systeme';
-import Testimonials from '@/components/Testimonials/Testimonials';
-import HomeComponent from '@/components/Home/Home.jsx';
+import Details2 from "@/components/Details/Details2";
+import Hero from "@/components/Hero/Hero";
+import Slogan from "@/components/Slogan/Slogan";
+import Stats from "@/components/Stats/Stats";
+import Systeme from "@/components/Systeme/Systeme";
+import Testimonials from "@/components/Testimonials/Testimonials";
+import HomeComponent from "@/components/Home/Home.jsx";
 
 export const metadata = {
-  title: ' Acceuil | Intello',
+  title: "Accueil | Intello",
 };
 export default function Home() {
   return (
     <HomeComponent>
-      <Hero><Slogan/></Hero>
-        <Details2 />
-        <Details />
-        <Systeme />
-        <Stats />
-        <Testimonials/>
-        <Contact/>
+      <Hero>
+        <Slogan />
+      </Hero>
+      <Details2 />
+      <Details />
+      <Systeme />
+      <Stats />
+      <Testimonials />
+      <Contact />
     </HomeComponent>
   );
 }
